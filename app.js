824e895228e8e27e8a233b0ce4b3a88cfd4e3b09
jopoll87 <poll.jonathan87@gmail.com>
2023-07-29 15:32:43 -0600

    half way done

--- app.js
+++ app.js
@@ -1,9 +1,48 @@
-const express = require('express');
+const path = require("path");
 
-const authRoutes = require('./routes/auth.routes')
+const express = require("express");
+const csrf = require('csurf');
+const expressSession = require('express-session');
+
+const createSessionConfig = require('./config/session');
+const db = require("./data/database");
+const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
+const checkAuthStatusMiddleware = require('./middlewares/check-auth');
+const authRoutes = require("./routes/auth.routes");
+const productsRoutes = require('./routes/products.routes')
+const baseRoutes = require('./routes/base.routes');
+const adminRoutes = require('./routes/admin.routes');
 
 const app = express();
 
-app.use(authRoutes);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.listen(3000);
+app.use(express.static("public"));
+app.use('/products/assets', express.static('product-data'));
+app.use(express.urlencoded({ extended: false }));
+
+const sessionConfig = createSessionConfig();
+
+app.use(expressSession(sessionConfig));
+app.use(csrf());
+
+app.use(addCsrfTokenMiddleware);
+app.use(checkAuthStatusMiddleware);
+
+app.use(baseRoutes);
+app.use(authRoutes);
+app.use(productsRoutes);
+app.use('/admin', adminRoutes);
+
+app.use(errorHandlerMiddleware);
+
+db.connectToDatabase()
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log("Failed to connect to the database!");
+    console.log(eror);
+  });
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,12 +1,157 @@
-const getSignUp = (req, res) => {
-    //...
-}
+const User = require("../models/user.model");
+const authUtil = require("../util/authentication");
+const validation = require("../util/validation");
+const sessionFlash = require("../util/session-flash");
+const session = require("express-session");
 
-const getLogin = (req,res) => {
-    // ...
-}
+const getSignUp = (req, res) => {
+  let sessionData = sessionFlash.getSessionData(req);
+
+  if (!sessionData) {
+    sessionData = {
+      email: "",
+      confirmEmail: "",
+      password: "",
+      fullname: "",
+      street: "",
+      postal: "",
+      city: "",
+    };
+  }
+
+  res.render("../views/customer/auth/signup", { inputData: sessionData });
+};
+
+const signup = async (req, res, next) => {
+  const enteredData = {
+    email: req.body.email,
+    confirmEmail: req.body['confirm-email'],
+    password: req.body.password,
+    fullname: req.body.fullname,
+    street: req.body.street,
+    postal: req.body.postal,
+    city: req.body.city,
+  };
+  if (
+    !validation.userDetailsAreValid(
+      req.body.email,
+      req.body.password,
+      req.body.fullname,
+      req.body.street,
+      req.body.postal,
+      req.body.city
+    ) ||
+    !validation.emailIsConfirmed(req.body.email, req.body["confirm-email"])
+  ) {
+    sessionFlash.flashDataToSession(
+      req,
+      {
+        errorMessage:
+          "Please check your input. Password must be at least 6 characters long, Postal code must be at least 5 charactes long.",
+        ...enteredData,
+      },
+      () => {
+        res.redirect("/signup");
+      }
+    );
+    return;
+  }
+
+  const user = new User(
+    req.body.email,
+    req.body.password,
+    req.body.fullname,
+    req.body.street,
+    req.body.postal,
+    req.body.city
+  );
+
+  try {
+    const exsistsAlready = await user.existsAlready();
+
+    if (exsistsAlready) {
+      sessionFlash.flashDataToSession(
+        req,
+        {
+          errorMessage: "User exists already! Try logging in instead!",
+          ...enteredData,
+        },
+        () => {
+          res.redirect("/signup");
+        }
+      );
+      return;
+    }
+
+    await user.signup();
+  } catch (error) {
+    return next(error);
+  }
+
+  res.redirect("/login");
+};
+
+const getLogin = (req, res) => {
+  let sessionData = sessionFlash.getSessionData(req);
+
+  if(!sessionData) {
+    sessionData = {
+      email: '',
+      password: '',
+    }
+  }
+
+  res.render("customer/auth/login", { inputData: sessionData });
+};
+
+const login = async (req, res, next) => {
+  const user = new User(req.body.email, req.body.password);
+  let exsistingUser;
+  try {
+    exsistingUser = await user.getUserWithSameEmail();
+  } catch (error) {
+    return next(error);
+  }
+
+  const sessionErrorData = {
+    errorMessage:
+      "Invalid credentials - please double-check your email and password!",
+    email: user.email,
+    password: user.password,
+  };
+
+  if (!exsistingUser) {
+    sessionFlash.flashDataToSession(req, sessionErrorData, () => {
+      res.redirect("/login");
+    });
+    return;
+  }
+
+  const passwordIsCorrect = await user.hasMatchingPassword(
+    exsistingUser.password
+  );
+
+  if (!passwordIsCorrect) {
+    sessionFlash.flashDataToSession(req, sessionErrorData, () => {
+      res.redirect("/login");
+    });
+    return;
+  }
+
+  authUtil.createUserSession(req, exsistingUser, () => {
+    res.redirect("/");
+  });
+};
+
+const logout = (req, res) => {
+  authUtil.destroyUserAuthSession(req);
+  res.redirect("/login");
+};
 
 module.exports = {
-    getSignUp: getSignUp,
-    getLogin: getLogin
-}
+  getSignUp: getSignUp,
+  getLogin: getLogin,
+  signup: signup,
+  login: login,
+  logout: logout,
+};
